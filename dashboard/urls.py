241bca81b5df7a81a4ae1b61afa4da364e45fe89
BhavyaSharma0212 <82492348+BhavyaSharma0212@users.noreply.github.com>
2022-06-30 10:49:10 +0530

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.views.static import serve
-from django.conf.urls import url
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('predictor.urls')),
 
-    path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
